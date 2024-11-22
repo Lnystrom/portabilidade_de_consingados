20147a3015bda0400b1901b3ece914accc3c4a9c
nystrom <apleuryssomrs@gmail.com>
2024-11-22 20:39:25 -0300

    Criado o executável

---
+++ setup.py
@@ -0,0 +1,15 @@
+from cx_Freeze import setup, Executable
+
+# Defina os detalhes do seu script Python
+setup(
+    name="Calculador de Saldo de Liquidação",
+    version="1.0",
+    description="Este aplicativo calcula automaticamente o saldo de liquidação e a taxa de juros de empréstimos consignados do INSS.",
+    options={
+        "build_exe": {
+            # Inclua os arquivos necessários (por exemplo, gui.py)
+            "include_files": ["gui.py"],
+        }
+    },
+    executables=[Executable("main.py")]
+)
