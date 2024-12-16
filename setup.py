11a4329e0ab0b9279361a099c57453919f6e7113
nystrom <apleuryssomrs@gmail.com>
2024-12-15 23:15:23 -0300

    setup configurado com sucesso

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from cx_Freeze import setup, Executable
+import os
+
+# Caminho da pasta e arquivo que você quer incluir
+imagem = os.path.join(os.getcwd(), "tabela_liquidacao.png")  # Caminho do arquivo de imagem
+pasta_padrao = os.path.join(os.getcwd(), "padrao")  # Caminho da pasta que você quer incluir
 
 # Defina os detalhes do seu script Python
 setup(
@@ -7,10 +12,15 @@ setup(
     description="Este aplicativo calcula automaticamente o saldo de liquidação e a taxa de juros de empréstimos consignados do INSS.",
     options={
         "build_exe": {
-            # Inclua os arquivos necessários (por exemplo, gui.py)
+            # Inclua os arquivos necessários (imagem e pasta)
+            "include_files": [
+                imagem,    # Inclui a imagem
+                pasta_padrao  # Inclui a pasta
+            ]
         }
     },
     executables=[Executable("nova_gui.py", icon="icone.ico")]  # Aqui é onde você adiciona o ícone
 )
 
-#python setup.py build
+# Para rodar, você utiliza o comando:
+# python setup.py build
